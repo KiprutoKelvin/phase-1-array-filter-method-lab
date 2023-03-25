f457335925075354078ead07d47a6f415e0d4ef1
Kiru <kelvin.kipruto@student.moringaschool.com>
2023-03-26 00:19:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+ function fuzzyMatch(drivers, query) {
+    if (!query || typeof query !== 'string') {
+      return [];
+    }
+    
+    return drivers.filter(function(driver) {
+      const partialMatch = driver.slice(0, query.length);
+      return partialMatch.toLowerCase() === query.toLowerCase();
+    });
+}
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
